902884bcd7df4ccc431cc3dca8b736d91e0f55b5
Abhi Khanal <khanal.abhi@gmail.com>
2016-07-02 20:08:23 -0400

    added local db connection.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var MongoClient = require('mongodb').MongoClient;
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -22,39 +24,58 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
+MongoClient.connect("mongodb://localhost:27017/data_db", function (err, db) {
+// MongoClient.connect("mongodb://localhost:27017/data_db", function (err, db) {
+  if(err){
+    console.println("error connecting to the database!");
+  } else {
+    console.log("connection established successfully!");
+
+    app.use('/', routes);
+    app.use('/users', users);
+
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+      var err = new Error('Not Found');
+      err.status = 404;
+      next(err);
+    });
+
+    // add the db connection
+
+    app.use(function (req, res, next) {
+      req.db = db;
+      next();
+    });
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+    if (app.get('env') === 'development') {
+      app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+          message: err.message,
+          error: err
+        });
+      });
+    }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: {}
+      });
+    });
 
+
+
+  }
+});
 
 module.exports = app;
