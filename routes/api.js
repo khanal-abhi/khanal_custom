8ebbfa47376bdc091ba54ff29cfabcacd2fb3ca7
Abhi Khanal <khanal.abhi@gmail.com>
2016-07-04 23:10:07 -0400

    updated the api

--- routes/api.js
+++ routes/api.js
@@ -9,48 +9,38 @@ var router = express.Router();
 
 router.post('/v1/new_message', function (req, res, next) {
 
-    if(req.session && req.session.user) {
-        var one_liner = {};
-        one_liner.message = req.body.message;
-        one_liner.author = req.session.user.username;
-        one_liner.popularity = 0;
-        one_liner.timestamp = Date.now();
+    var one_liner = {};
+    one_liner.message = req.body.message;
+    one_liner.author = req.session.user.username;
+    one_liner.popularity = 0;
+    one_liner.timestamp = Date.now();
 
-        var err = [];
-        var db = req.db;
+    var err = [];
+    var db = req.db;
 
-        if (one_liner.message == null || one_liner.message == "") {
-            err.push("missing message");
-        }
-
-        if (one_liner.author == null || one_liner.author == "") {
-            err.push("missing author");
-        }
-
-        if (err.length > 0) {
-            res.send({'error': err});
-        }
-
-
-        var one_liners = db.collection("one_liners");
-        one_liners.insert(one_liner, function (err, doc) {
-            if (err) {
-                console.log('error saving one liner ' + err);
-            }
-            else {
-                console.log('saved one_liner successfully!');
-                res.send(one_liner);
-            }
-        });
-    } else {
-
-        res.send({
-            'result': 'unsuccessful',
-            'error': 'not authenticated'
-        });
-
+    if (one_liner.message == null || one_liner.message == "") {
+        err.push("missing message");
     }
 
+    if (one_liner.author == null || one_liner.author == "") {
+        err.push("missing author");
+    }
+
+    if (err.length > 0) {
+        res.send({'error': err});
+    }
+
+
+    var one_liners = db.collection("one_liners");
+    one_liners.insert(one_liner, function (err, doc) {
+        if (err) {
+            console.log('error saving one liner ' + err);
+        }
+        else {
+            console.log('saved one_liner successfully!');
+            res.send(one_liner);
+        }
+    });
 
 });
 
